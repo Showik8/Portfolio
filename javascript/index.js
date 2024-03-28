00abd435fe85a7181943d6f1a291d6a74a0d94a6
Showik8 <shotaabashidze88@gmail.com>
2024-03-29 02:02:25 +0400

    First Commit

---
+++ javascript/index.js
@@ -0,0 +1,60 @@
+
+const conainer = document.getElementById('container')
+const myDiv = document.createElement(`div`)
+const myH2 = document.createElement(`h2`)
+const myButton = document.createElement(`button`)
+const myP = document.createElement(`p`)
+const myInput = document.createElement(`input`)
+
+function makingContainer(){
+    myH2.innerText ="Es sheiqmna JsShi"
+    myButton.innerText="Test Button"
+    myButton.addEventListener("click", Testing)
+    myP.innerText ="Daakliket"
+    myDiv.classList.add(`test`)
+    myDiv.appendChild(myH2)
+    myDiv.appendChild(myP)
+    myDiv.appendChild(myButton)
+
+    conainer.appendChild(myDiv)
+
+}
+makingContainer()
+
+ 
+
+function makingSecondCont(){
+    let secCon = document.createElement(`div`)
+    secCon.classList.add(`secCon`)
+   
+    const myButton2=document.createElement(`button`) 
+    myH2.innerText = "woow It's Working"
+    secCon.appendChild(myH2)
+    setTimeout(()=>{
+        myH2.innerText = "Okay Next Step"
+        myButton2.innerText= "Click Me"
+        myButton2.addEventListener("click", () => {
+            myH2.classList.add('hidden')
+            myButton2.classList.add('hidden')
+            myInput.classList.remove('hidden')
+            
+            })
+        secCon.appendChild(myButton2) 
+    },5000)  
+    myInput.classList.add('hidden')
+    myInput.inputMode= `text`
+    myInput.placeholder=`Enter Your Name Here`
+    secCon.appendChild(myInput)    
+    document.body.appendChild(secCon)
+}
+
+
+function Testing(){
+    conainer.classList.add( `hidden`);
+ 
+   setTimeout(() => {
+        makingSecondCont();
+    }, 1000);
+    
+}
+
