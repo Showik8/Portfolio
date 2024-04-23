59f697a66f3d74363283b3b6f8151eb85e66dc43
Shota_Abashidze <shotaabashidze88@gmail.com>
2024-04-23 10:35:40 +0400

    fixed

--- github/index.js
+++ github/index.js
@@ -1,32 +1,29 @@
-const list = document.getElementById('list');
 const searchInput = document.getElementById('searchInput');
+let inputcont = document.getElementById(`inputcont`)
+let backcont =document.getElementById(`backcont`)
+
+const userlogin = document.getElementById(`login`)
+const img = document.getElementById(`img`)
+const username =document.getElementById(`username`)
+const notFound = document.getElementById(`notFound`);
+const repos = document.getElementById(`repos`)
+const follower = document.getElementById(`follower`)
+const following = document.getElementById(`following`)
+const html = document.getElementById(`html`)
+const maincont = document.getElementById(`mainCont`)
+const createTime = document.getElementById(`createTime`)
+const userlocation = document.getElementById(`userlocation`)
+const office = document.getElementById(`office`)
+const twt = document.getElementById('twt')
+const bio = document.getElementById(`bio`) 
+
 
 
 searchInput.addEventListener('keyup', handleChange)
 
-function searchOld(keyword) {
-  const data = [
-  ];
-
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const result = data.filter(function(text){
-        if (text.includes(keyword)) {
-          return true
-        }
-        return false
-      })
-      resolve(result);
-    }, 150)
-  })
-  
-}
-
-
-
 async function search(keyword) {
   return fetch(
-    `https://api.github.com/search/users?q=${keyword}`, 
+    `https://api.github.com/users/${keyword}`,
     {
       headers: {
         "Accept": "application/vnd.github+json",
@@ -38,50 +35,63 @@ async function search(keyword) {
   .then(res => res.json())
 }
 
-async function handleChange(e) {
-  if (e.target.value.length <= 5) return
+async function handleChange(e) { 
+  if(e.target.value >=4) return
   const data = await search(e.target.value)
   console.log(data)
-  const oldResult = list.querySelectorAll('li');
-  oldResult.forEach(e => e.remove());
-  if (data.items.length === 0) {
-    const notFound = document.createElement('li');
-    notFound.innerText = "Items with matching text not found!!!";
-    notFound.classList.add('notFound');
-    list.appendChild(notFound);
-  }
-  data.items.forEach(function(value){
-    debugger
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    const p = document.createElement('p');
-    const open= document.createElement('button');
-    open.addEventListener(`click` ,ramefuncqia)
-    open.classList.add("smallBtn")
-    img.src = value.avatar_url;
-    p.innerText = value.login;
-    li.appendChild(img);
-    li.appendChild(p);
-    li.appendChild(open)
-    list.appendChild(li);
-  })
-
+  document.body.removeChild(backcont)
+  maincont.style.display=`flex`
+  username.innerText = data.name
+  username.append()
+  img.src = data.avatar_url
+  img.append()
+  userlogin.innerText=`@ ${data.login}`
+  userlogin.append()
+  follower.innerText= data.followers_url.length
+  follower.append()
+  following.innerText = data.following_url.length
+  following.append()
+  repos.innerText = data.repos_url.length
+  repos.append()
+  let year = data.created_at.slice(0,4)
+  let month = data.created_at.slice(5,7)
+  let day = data.created_at.slice(8,10)
+  createTime.innerText =`joined ${day}/${month}/${year}`
+  createTime.append()
+  html.innerText= data.html_url
+  userlocation.innerText=data.location
+  userlocation.append()
+  office.innerText=data.company
+  office.append()
+  twt.innerText=data.twitter_username
+  twt.append()
+  bio.innerText=data.bio
+  bio.append()
 }
 
 
+let btn = document.getElementById(`btn`)
+btn.addEventListener(`click`,  darkMode)
 
-new Promise((resolve, reject) => {
-  resolve('hello')
-}).then(res => {
-  setTimeout(() => {
-    console.log(res)
-  }, 0)
-})
 
-setTimeout(() => {
-  console.log(3)
-}, 0)
 
-function ramefuncqia() {
-  console.log("wpw")
+function  darkMode() {
+  let mode =document.body.classList.toggle(`light`)
+  let darcont = document.getElementById(`darkMode`) 
+  let span = document.getElementById(`darkText`)
+  let icon = document.getElementById(`icon`)
+   
+  if(mode==true){
+      span.innerText="DARK"
+      icon.src =`./assets/Path.png`
+    }else{
+      span.innerText="LIGTH"
+      icon.src =`./assets/sun.png`
+    }
+    darcont.classList.toggle(`headContLight`)
+    inputcont.classList.toggle(`inputcontLigth`)
+    backcont.classList.toggle(`backcontLigth`)
+   
+
 }
+
--- github/main.js
+++ github/main.js
@@ -1,6 +1,6 @@
-const searchInput = document.getElementById(`searchInput`);
-searchInput.addEventListener(`keyup`, handleChange)
-
+let inputcont = document.getElementById(`inputcont`)
+let backcont =document.getElementById(`backcont`)
+const userlogin = document.getElementById(`login`)
 const img = document.getElementById(`img`)
 const username =document.getElementById(`username`)
 const notFound = document.getElementById(`notFound`);
@@ -8,60 +8,135 @@ const repos = document.getElementById(`repos`)
 const follower = document.getElementById(`follower`)
 const following = document.getElementById(`following`)
 const html = document.getElementById(`html`)
-const clearcont = document.getElementById(`backcont`)
 const maincont = document.getElementById(`mainCont`)
+const createTime = document.getElementById(`createTime`)
+const userlocation = document.getElementById(`userlocation`)
+const office = document.getElementById(`office`)
+const twt = document.getElementById('twt')
+const bio = document.getElementById(`bio`) 
 
-async function search(valueInput) {
-  return fetch(
-      `https://api.github.com/users?q=${valueInput}`, 
-      {
-        headers: {
-          "Accept": "application/vnd.github+json",
-          "Authorization": "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",
-          "X-GitHub-Api-Version": "2022-11-28"
-        }
+
+
+
+async function fetchdata(username){
+    return fetch(
+    `https://api.github.com/search/users?q=${username}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "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",
+        "X-GitHub-Api-Version": "2022-11-28"
       }
-    )
-    .then(res => res.json())
-  }
-  
-// async function getdata(valueInput){
-//     return fetch(
-//       `https://api.github.com/users?q=${valueInput}`, 
-//       {
-//         headers: {
-//           "Accept": "application/vnd.github+json",
-//           "Authorization": "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",
-//           "X-GitHub-Api-Version": "2022-11-28"
-//         }
+    }
+  )
+  .then(res => res.json())
+}
+
+async function search(){
+  let searchInput = document.getElementById('searchInput').value;
+  const ulList = document.getElementById(`ul`)
+  document.body.classList.remove(`start`)
+  const result= await fetchdata(searchInput)
+  console.log(result.items[0].login)
+
+  // for (let i=0; result.items[i]; i++){
+  //  if(i<5){
+  //   const li = document.createElement(`li`)
+  //   const h1 = document.createElement(`h1`) 
+  //   const profilePhoto = document.createElement(`img`)
+  //   const button = document.createElement(`button`)
+  //   h1.innerText=result.items[i].login
+  //   profilePhoto.src=result[i].avatar_url
+  //     li.appendChild(profilePhoto)
+  //     li.appendChild(h1)
+  //     li.appendChild(button)
+  //     ulList.appendChild(li)
+  //      }
+  //    if(i==5){
+  //     break
+  //   }
+  // }
+}
+
+
+
+
+
+
+// searchInput.addEventListener('keyup', handleChange)
+
+
+// async function fetche(keyword) {
+//   return fetch(
+//     `https://api.github.com/users/${keyword}`,
+//     {
+//       headers: {
+//         "Accept": "application/vnd.github+json",
+//         "Authorization": "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",
+//         "X-GitHub-Api-Version": "2022-11-28"
 //       }
-//     )
-//     .then(res => res.json())
-//   }
+//     }
+//   )
+//   .then(res => res.json())
+// }
+
+// async function handleChange(e) { 
+//   if(e.target.value >=4) return
+//   const data = await fetche(e.target.value)
+//   console.log(data)
+//   document.body.removeChild(backcont)
+//   maincont.style.display=`flex`
+//   username.innerText = data.name
+//   username.append()
+//   img.src = data.avatar_url
+//   img.append()
+//   userlogin.innerText=`@ ${data.login}`
+//   userlogin.append()
+//   follower.innerText= data.followers_url.length
+//   follower.append()
+//   following.innerText = data.following_url.length
+//   following.append()
+//   repos.innerText = data.repos_url.length
+//   repos.append()
+//   let year = data.created_at.slice(0,4)
+//   let month = data.created_at.slice(5,7)
+//   let day = data.created_at.slice(8,10)
+//   createTime.innerText =`joined ${day}/${month}/${year}`
+//   createTime.append()
+//   html.innerText= data.html_url
+//   userlocation.innerText=data.location
+//   userlocation.append()
+//   office.innerText=data.company
+//   office.append()
+//   twt.innerText=data.twitter_username
+//   twt.append()
+//   bio.innerText=data.bio
+//   bio.append()
+// }
 
 
-  async function handleChange(e) {
-    // if (e.target.value.length >= 4) return
-    // const data = await search(e.target.value)
-    // // console.log(data)
+// let btn = document.getElementById(`btn`)
+// btn.addEventListener(`click`,  darkMode)
 
-    while(e.target.length == 1 )return
-    const result = await search(e.target.value)
-    console.log(result)
-    // const user = await getdata(e.target.value)
-    // console.log(user)
-    // const smalldata =result.items[0]
-    // img.src = smalldata.avatar_url
-    // username.innerText = smalldata.login
-    // repos.innerText = smalldata.repos_url.length
-    // follower.innerText= smalldata.followers_url.length
-    // following.innerText = smalldata.following_url.length
-    // html.innerText= smalldata.html_url
 
-    // if (data.items.length === 0) {
-    //   notFound.innerText = "No result" 
-    // }
-    
-  }
-  
- 
+
+// function  darkMode() {
+//   let mode =document.body.classList.toggle(`light`)
+//   let darcont = document.getElementById(`darkMode`) 
+//   let span = document.getElementById(`darkText`)
+//   let icon = document.getElementById(`icon`)
+   
+//   if(mode==true){
+//       span.innerText="DARK"
+//       icon.src =`./assets/Path.png`
+//     }else{
+//       span.innerText="LIGTH"
+//       icon.src =`./assets/sun.png`
+//     }
+//     darcont.classList.toggle(`headContLight`)
+//     inputcont.classList.toggle(`inputcontLigth`)
+//     backcont.classList.toggle(`backcontLigth`)
+   
+
+// }
+
