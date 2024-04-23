a2276b20a2e1e9c0379712c3843ed78acc9f136d
Showik8 <shotaabashidze88@gmail.com>
2024-04-23 19:40:48 +0400

    push

--- github/main.js
+++ github/main.js
@@ -14,6 +14,24 @@ const userlocation = document.getElementById(`userlocation`)
 const office = document.getElementById(`office`)
 const twt = document.getElementById('twt')
 const bio = document.getElementById(`bio`) 
+const searchInput = document.getElementById(`searchInput`)
+
+function smallSearch(arg){
+  for (let i=0; result.items[i]; i++){
+    if(i<5){
+     h1.innerText=result.items[i].login
+     profilePhoto.src=result[i].avatar_url
+       li.appendChild(profilePhoto)
+       li.appendChild(h1)
+       li.appendChild(button)
+       ulList.appendChild(li)
+        }
+      if(i==5){
+       break
+     }
+   }
+
+}
 
 
 
@@ -34,51 +52,131 @@ async function fetchdata(username){
 
 async function search(){
   let searchInput = document.getElementById('searchInput').value;
-  const ulList = document.getElementById(`ul`)
   document.body.classList.remove(`start`)
   const result= await fetchdata(searchInput)
-  console.log(result.items[0].login)
+  console.log(result.total_count)
+  const li = document.createElement(`li`)
+  const h1 = document.createElement(`h1`) 
+  const profilePhoto = document.createElement(`img`)
+  const button = document.createElement(`button`)
+  const ulList = document.getElementById(`list`)   
+  button.innerText= `click`
+  button.addEventListener(`click`, liButton)
+  
+  for (let i=0; result.items[i]; i++){
+    if(i<=5){
 
-  // for (let i=0; result.items[i]; i++){
-  //  if(i<5){
-  //   const li = document.createElement(`li`)
-  //   const h1 = document.createElement(`h1`) 
-  //   const profilePhoto = document.createElement(`img`)
-  //   const button = document.createElement(`button`)
-  //   h1.innerText=result.items[i].login
-  //   profilePhoto.src=result[i].avatar_url
-  //     li.appendChild(profilePhoto)
-  //     li.appendChild(h1)
-  //     li.appendChild(button)
-  //     ulList.appendChild(li)
-  //      }
-  //    if(i==5){
-  //     break
-  //   }
-  // }
+      console.log(`is ${i}`)
+     h1.innerText=result.items[i].login
+     profilePhoto.src=result.items[i].avatar_url
+       li.appendChild(profilePhoto)
+       li.appendChild(h1)
+       li.appendChild(button)
+       ulList.appendChild(li)
+        }
+     if(i==6){
+      break
+     }
+   }
+
+   if(result.total_count==0){
+    notFound.innerText=`not founded`
+    notFound.append()
+   }
+
+
+    result.items.forEach(function(element){
+        console.log(element)
+      
+      h1.innerText=element.login
+        h1.setAttribute(`id`, `useruser`)
+      profilePhoto.src=element.avatar_url
+      li.appendChild(profilePhoto)
+      li.appendChild(h1)
+      li.appendChild(button)
+      ulList.appendChild(li)
+      
+    });
+  
+}
+
+
+async function searchForDiv(keyword) {
+  return fetch( 
+    `https://api.github.com/users/${keyword}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "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",
+        "X-GitHub-Api-Version": "2022-11-28"
+      }
+    }
+  )
+  .then(res => res.json())
 }
 
 
 
+ async function liButton(){
+    let gitname = document.getElementById(`useruser`).innerText
+    const data= await searchForDiv(gitname)
+    console.log(data)
+    document.body.removeChild(backcont)
+    maincont.style.display=`flex`
+    maincont.classList.remove(`hidden`)
+    maincont.classList.add(`cont`)
+    username.innerText = data.name
+    username.append()
+    img.src = data.avatar_url
+    img.append()
+    userlogin.innerText=`@ ${data.login}`
+    userlogin.append()
+    follower.innerText= data.followers_url.length
+    follower.append()
+    following.innerText = data.following_url.length
+    following.append()
+    repos.innerText = data.repos_url.length
+    repos.append()
+    let year = data.created_at.slice(0,4)
+    let month = data.created_at.slice(5,7)
+    let day = data.created_at.slice(8,10)
+    createTime.innerText =`joined ${day}/${month}/${year}`
+    createTime.append()
+    html.innerText= data.html_url
+    userlocation.innerText=data.location
+    userlocation.append()
+    office.innerText=data.company
+    office.append()
+    twt.innerText=data.twitter_username
+    twt.append()
+    bio.innerText=data.bio
+    bio.append()
+}
 
 
+searchInput.addEventListener('keyup', handleChange)
 
-// searchInput.addEventListener('keyup', handleChange)
+function handleChange(e){
+  if (e.target.value >=4 ){
+    maincont.classList.add('hide')
+    document.body.appendChild(backcont)
+  }
+}
 
 
-// async function fetche(keyword) {
-//   return fetch(
-//     `https://api.github.com/users/${keyword}`,
-//     {
-//       headers: {
-//         "Accept": "application/vnd.github+json",
-//         "Authorization": "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",
-//         "X-GitHub-Api-Version": "2022-11-28"
-//       }
-//     }
-//   )
-//   .then(res => res.json())
-// }
+async function fetche(keyword) {
+  return fetch(
+    `https://api.github.com/users/${keyword}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "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",
+        "X-GitHub-Api-Version": "2022-11-28"
+      }
+    }
+  )
+  .then(res => res.json())
+}
 
 // async function handleChange(e) { 
 //   if(e.target.value >=4) return
@@ -115,28 +213,27 @@ async function search(){
 // }
 
 
-// let btn = document.getElementById(`btn`)
-// btn.addEventListener(`click`,  darkMode)
+let btn = document.getElementById(`btn`)
+btn.addEventListener(`click`,  darkMode)
 
 
 
-// function  darkMode() {
-//   let mode =document.body.classList.toggle(`light`)
-//   let darcont = document.getElementById(`darkMode`) 
-//   let span = document.getElementById(`darkText`)
-//   let icon = document.getElementById(`icon`)
+function  darkMode() {
+  let mode =document.body.classList.toggle(`light`)
+  let darcont = document.getElementById(`darkMode`) 
+  let span = document.getElementById(`darkText`)
+  let icon = document.getElementById(`icon`)
    
-//   if(mode==true){
-//       span.innerText="DARK"
-//       icon.src =`./assets/Path.png`
-//     }else{
-//       span.innerText="LIGTH"
-//       icon.src =`./assets/sun.png`
-//     }
-//     darcont.classList.toggle(`headContLight`)
-//     inputcont.classList.toggle(`inputcontLigth`)
-//     backcont.classList.toggle(`backcontLigth`)
+  if(mode==true){
+      span.innerText="DARK"
+      icon.src =`./assets/Path.png`
+    }else{
+      span.innerText="LIGTH"
+      icon.src =`./assets/sun.png`
+    }
+    darcont.classList.toggle(`headContLight`)
+    inputcont.classList.toggle(`inputcontLigth`)
+    backcont.classList.toggle(`backcontLigth`)
    
 
-// }
-
+}
