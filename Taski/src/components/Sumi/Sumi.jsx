0abbe398706e61e39ee3f358d83004d38b847e7f
Showik8 <shotaabashidze88@gmail.com>
2024-06-06 20:33:17 +0400

    Wow

--- Taski/src/components/Sumi/Sumi.jsx
+++ Taski/src/components/Sumi/Sumi.jsx
@@ -8,10 +8,10 @@ export const Sumi =()=>{
         <div className="summaryDiv">
             <span> Summary</span>
             <div className="cards">
-            <Card icon="../../assets/Foto.png" BgColor="rgba(255, 85, 85, 0.05)" color="rgba(255, 85, 85, 1)" res="80" text = "Reaction" />
-            <Card color="rgba(255, 178, 30, 1)" BgColor="rgba(255, 178, 30, 0.05)" res="92" text ="Memory"/>
-            <Card BgColor="rgba(0, 187, 143, 0.05)" color="green" res="61" text ="Verbal"/>
-            <Card BgColor="rgba(17, 37, 214, 0.05)" color="blue" res="80" text ="Visual" />
+            <Card  icon="/src/assets/Foto.png" BgColor="rgba(255, 85, 85, 0.05)" color="rgba(255, 85, 85, 1)" res="80" text = "Reaction" />
+            <Card icon="/src/assets/brain.png" color="rgba(255, 178, 30, 1)" BgColor="rgba(255, 178, 30, 0.05)" res="92" text ="Memory"/>
+            <Card icon="/src/assets/Vector.png" BgColor="rgba(0, 187, 143, 0.05)" color="green" res="61" text ="Verbal"/>
+            <Card  icon="/src/assets/ver.png" BgColor="rgba(17, 37, 214, 0.05)" color="blue" res="80" text ="Visual" />
             </div>
             <button className="btn">Continue</button>
         </div>
