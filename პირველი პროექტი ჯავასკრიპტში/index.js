009acf1a984a0397c95ca379ab66a34f62a2b7a0
Showik8 <shotaabashidze88@gmail.com>
2025-05-08 08:49:47 +0400

    small Error

--- პირველი პროექტი ჯავასკრიპტში/index.js
+++ პირველი პროექტი ჯავასკრიპტში/index.js
@@ -1,69 +1,61 @@
-const conainer = document.getElementById('container')
-const myDiv = document.createElement(`div`)
-const myH2 = document.createElement(`h2`)
-const myButton = document.createElement(`button`)
-const myP = document.createElement(`p`)
-const myInput = document.createElement(`input`)
-myInput.setAttribute("id",`myId`)
+const conainer = document.getElementById("container");
+const myDiv = document.createElement(`div`);
+const myH2 = document.createElement(`h2`);
+const myButton = document.createElement(`button`);
+const myP = document.createElement(`p`);
+const myInput = document.createElement(`input`);
+myInput.setAttribute("id", `myId`);
 
-function makingContainer(){
-    myH2.innerText ="Es sheiqmna JsShi"
-    myButton.innerText="Okay"
-    myButton.addEventListener("click", Testing)
-    myP.innerText ="Daakliket"
-    myDiv.classList.add(`test`)
-    myDiv.appendChild(myH2)
-    myDiv.appendChild(myP)
-    myDiv.appendChild(myButton)
-    conainer.appendChild(myDiv)
-
+function makingContainer() {
+  myH2.innerText = "Es sheiqmna JsShi";
+  myButton.innerText = "Okay";
+  myButton.addEventListener("click", Testing);
+  myP.innerText = "Daakliket";
+  myDiv.classList.add(`test`);
+  myDiv.appendChild(myH2);
+  myDiv.appendChild(myP);
+  myDiv.appendChild(myButton);
+  conainer.appendChild(myDiv);
 }
 
- 
-
-function makingSecondCont(){
-    let secCon = document.createElement(`div`)
-    secCon.classList.add(`secCon`)
-    let buttonForInput=document.createElement(`button`);
-    buttonForInput.classList.add("hidden")
-    const myButton2=document.createElement(`button`) 
-    myH2.innerText = "woow It's Working"
-    secCon.appendChild(myH2)
-    setTimeout(()=>{
-        myH2.innerText = "Okay Next Step"
-        myButton2.innerText= "Click Me"
-        myButton2.addEventListener("click", () => {
-            myH2.classList.add('hidden')
-            myButton2.classList.add('hidden')
-            myInput.classList.remove('hidden')
-            buttonForInput.classList.remove(`hidden`)
-            
-            })
-        secCon.appendChild(myButton2) 
-    },5000)  
-    myInput.classList.add('hidden')
-    myInput.inputMode= `text`
-    myInput.placeholder=`Enter Your Name Here`
-    secCon.appendChild(myInput)   
-    // let buttonForInput=document.createElement(`button`);
-    buttonForInput.innerText="Submit";
-   
-    buttonForInput.setAttribute("id","submit-btn")
-    buttonForInput.addEventListener("click",()=>{
-       console.log(`Hello ${myInput.value}`
-       localStorage.getItem("myInput.value"));
+function makingSecondCont() {
+  let secCon = document.createElement(`div`);
+  secCon.classList.add(`secCon`);
+  let buttonForInput = document.createElement(`button`);
+  buttonForInput.classList.add("hidden");
+  const myButton2 = document.createElement(`button`);
+  myH2.innerText = "woow It's Working";
+  secCon.appendChild(myH2);
+  setTimeout(() => {
+    myH2.innerText = "Okay Next Step";
+    myButton2.innerText = "Click Me";
+    myButton2.addEventListener("click", () => {
+      myH2.classList.add("hidden");
+      myButton2.classList.add("hidden");
+      myInput.classList.remove("hidden");
+      buttonForInput.classList.remove(`hidden`);
     });
-    secCon.appendChild(buttonForInput);
-    document.body.appendChild(secCon)
+    secCon.appendChild(myButton2);
+  }, 5000);
+  myInput.classList.add("hidden");
+  myInput.inputMode = `text`;
+  myInput.placeholder = `Enter Your Name Here`;
+  secCon.appendChild(myInput);
+  // let buttonForInput=document.createElement(`button`);
+  buttonForInput.innerText = "Submit";
+
+  buttonForInput.setAttribute("id", "submit-btn");
+  buttonForInput.addEventListener("click", () => {
+    console.log(`Hello ${myInput.value}`);
+  });
+  secCon.appendChild(buttonForInput);
+  document.body.appendChild(secCon);
 }
 
+function Testing() {
+  conainer.classList.add(`hidden`);
 
-function Testing(){
-    conainer.classList.add( `hidden`);
- 
-   setTimeout(() => {
-        makingSecondCont();
-    }, 1000);
-    
+  setTimeout(() => {
+    makingSecondCont();
+  }, 1000);
 }
-
